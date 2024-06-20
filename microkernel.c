51438b67e1f118f02812138431c12699a2c29b7e
Gustavo Walbon <gustavowalbon@gmail.com>
2024-06-20 17:08:14 -0300

    WIP

--- microkernel.c
+++ microkernel.c
@@ -4,9 +4,9 @@
 #include <blis/blis.h>
 #include <math.h>
 
-#define M 512
-#define K 1024
-#define N 512
+#define M 8
+#define K 8
+#define N 8
 
 #define rand_min -5
 #define rand_max 5
@@ -19,7 +19,7 @@ int main(){
     float beta  = 0.0;
     float* A = (float *) malloc(M*K*sizeof(float));
     float* B = (float *) malloc(K*N*sizeof(float));
-    float* C_blas = (float *) calloc(M*N, sizeof(float));
+    float* C_blis = (float *) calloc(M*N, sizeof(float));
     float* C_naive = (float *) calloc(M*N, sizeof(float));
 
     //data initialization
@@ -36,7 +36,7 @@ int main(){
     }
     for(int i=0; i<M*N; i++){
             C_naive[i] = 0;
-            C_blas[i] = 0;
+            C_blis[i] = 0;
     }
 
     //openblas usage -- sanity check
@@ -47,25 +47,25 @@ int main(){
                A, /*rsa*/ K, /*csa*/ 1,
                B, /*rsb*/ N, /*csb*/ 1,
                &beta,
-               C_blas, /*rsc*/ N, /*csc*/ 1);
+               C_blis, /*rsc*/ N, /*csc*/ 1);
 
 
   int i, j, k;
     for (i=0; i<M; i++) {
         for (j=0; j<N; j++)
-            C_naive[i*M+j] *= beta;
+            C_naive[N*i+j] *= beta;
 
         for (k=0; k<K; k++) { 
             for (j=0; j<N; j++)
-                C_naive[i*M+j] += alpha * A[i*M+k] * B[j*N+k];
+                C_naive[N*i+j] += alpha * A[M*i+j] * B[N*j+k];
         }
     }
 
   int erro = 0;
   for (i = 0; i < M; i++) {
     for (j = 0; j < N; j++) {
-      if (abs(C_blas[i * M + j] - C_naive[i * M + j]) > 1e-6) {
-        printf("Error: [%d][%d] blas %f != %f naive\n", i, j, (float)C_blas[i*N + j], (float)C_naive[i*N+j], i, j);
+      if (abs(C_blis[i * M + j] - C_naive[i * M + j]) > 1e-6) {
+        //printf("Error: [%d][%d] blis %f != %f naive\n", i, j, (float)C_blis[i*N + j], (float)C_naive[i*N+j], i, j);
         erro += 1;
       }
     }
@@ -87,11 +87,18 @@ int main(){
         printf("%.0f ", B[i]);
     }
    */
-  if (erro) {
+   //Print C
+    printf("C_blis:\n");
+    for(int i=0; i<M; i++){
+        printf("C[%d][]: ", i);
+        for(int j=0; j<N; j++)
+            printf("%.0f ", C_blis[i*M+j]);
+        printf("\n");
+    }
+   if (erro) {
     printf("Validação da saída C falhou! %i vezes\n", erro);
     return 1;
-  } else {
+   } else {
     printf("Validação da saída C bem-sucedida!\n");
-  }
- 
+   }
 }
